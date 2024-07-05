0455ef1a761481a0d86969aa2d1b2887ce1ac86d
Julien Bonte <bonte.julien@gmail.com>
2024-07-05 21:04:47 +0200

    chore: Update deploy.sh script for automatic Go program deployment on Raspberry Pi

--- deploy.sh
+++ deploy.sh
@@ -4,48 +4,34 @@
 # Variables
 source .env
 
-# Vérification de la compilation du programme Go en local
-echo "Vérification de la compilation du programme Go en local..."
-go build -o /dev/null main.go
-
-if [ $? -ne 0 ]; then
-    echo "Échec de la compilation locale. Déploiement annulé."
-    exit 1
-fi
-
 # Synchronisation des fichiers
 echo "Synchronisation des fichiers..."
 rsync -avz --exclude='.git' $LOCAL_PATH $PI_USER@$PI_HOST:$PI_PATH
 
-# Arrêt du programme en cours sur le Raspberry Pi
-echo "Arrêt des processus 'go run main.go' en cours sur le Raspberry Pi..."
-ssh $PI_USER@$PI_HOST "pkill -f 'go run main.go'"
+# Arrêt du programme compilé en cours sur le Raspberry Pi
+echo "Arrêt des processus 'spot' en cours sur le Raspberry Pi..."
+ssh $PI_USER@$PI_HOST "pkill -f 'spot'"
 
 # Pause pour assurer que les processus sont arrêtés
 sleep 2
 
-# Vérification des processus arrêtés
-if ssh $PI_USER@$PI_HOST "pgrep -f 'go run main.go' > /dev/null"; then
-    echo "Les processus 'go run main.go' sont toujours en cours. Tentative d'arrêt forcé..."
-    ssh $PI_USER@$PI_HOST "pkill -9 -f 'go run main.go'"
-    sleep 2
-    if ssh $PI_USER@$PI_HOST "pgrep -f 'go run main.go' > /dev/null"; then
-        echo "Impossible d'arrêter les processus 'go run main.go'. Déploiement annulé."
-        exit 1
-    else
-        echo "Processus arrêtés avec succès après l'arrêt forcé."
-    fi
-else
-    echo "Tous les processus 'go run main.go' arrêtés avec succès."
+# Compilation du programme sur le Raspberry Pi
+echo "Compilation du programme sur le Raspberry Pi..."
+ssh $PI_USER@$PI_HOST "cd $PI_PATH && go build -o spot main.go"
+
+# Vérification de la compilation
+if ssh $PI_USER@$PI_HOST "[ ! -f $PI_PATH/spot ]"; then
+    echo "Échec de la compilation sur le Raspberry Pi. Déploiement annulé."
+    exit 1
 fi
 
-# Exécution du script sur le Raspberry Pi
-echo "Exécution du script sur le Raspberry Pi..."
-ssh $PI_USER@$PI_HOST "cd $PI_PATH && nohup go run main.go > /dev/null 2>&1 &"
+# Exécution du programme compilé sur le Raspberry Pi
+echo "Exécution du programme compilé sur le Raspberry Pi..."
+ssh $PI_USER@$PI_HOST "cd $PI_PATH && ./spot" &
 
 # Vérification que le nouveau processus est lancé
 sleep 2
-if ssh $PI_USER@$PI_HOST "pgrep -f 'go run main.go' > /dev/null"; then
+if ssh $PI_USER@$PI_HOST "pgrep -f './spot' > /dev/null"; then
     echo "Nouveau processus lancé avec succès"
 else
     echo "Échec du lancement du nouveau processus"
